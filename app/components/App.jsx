17259657726363e15156ed5b001920295a482e6f
HwangJaeHyuk <54526285+PoopooFaceMan@users.noreply.github.com>
2021-05-05 20:43:13 +0800

    completed with styling
    note: tables do not show proper bordering

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,10 +1,30 @@
 import React, { useCallback, useEffect, useState, Component } from "react";
 import { Switch, Route, useHistory } from "react-router-dom";
 import { connect } from "react-redux";
-import Sidebar from "./Sidebar.jsx";
+
 import AnalyticsDashboard from "./pages/AnalyticsDashboard/AnalyticsDashboard.jsx";
 import SingleWalkthroughAnalytics from "./pages/SingleWalkthroughAnalytics/SingleWalkthroughAnalytics.jsx";
 
+const sidebarstyle = {
+  background: "#3b3b3b",
+  float: "left",
+  paddingBottom: "100%",
+};
+const headerstyle = {
+  font: "Arial",
+  color: "#0b0b0b",
+  background: "#979797",
+  paddingTop: "5px",
+  paddingBottom: "5px",
+  paddingLeft: "40px",
+  paddingRight: "40px",
+};
+
+const positioning = {
+  margin: "auto",
+  width: "50%",
+  height: "50%",
+};
 class App extends Component {
   constructor(props) {
     super(props);
@@ -14,11 +34,17 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Sidebar />
-        <Switch>
-          <Route path="/analytics" component={AnalyticsDashboard} />
-          <Route path="/single" component={SingleWalkthroughAnalytics} />
-        </Switch>
+        <div>
+          <div style={sidebarstyle}>
+            <h2 style={headerstyle}>UserTip</h2>
+          </div>
+          <div style={positioning}>
+            <Switch>
+              <Route path="/analytics" component={AnalyticsDashboard} />
+              <Route path="/single" component={SingleWalkthroughAnalytics} />
+            </Switch>
+          </div>
+        </div>
       </div>
     );
   }
--- app/components/pages/SingleWalkthroughAnalytics/SingleWalkthroughAnalytics.jsx
+++ app/components/pages/SingleWalkthroughAnalytics/SingleWalkthroughAnalytics.jsx
@@ -14,46 +14,56 @@ const singleData = {
   errors_count: 67,
 };
 
-// const borderStyle = {
-//   border: "1px solid black",
-// };
-
+const tableborder = {
+  padding: "10px",
+  borderTop: "1px solid #dddddd",
+};
 const SingleWalkthroughAnalytics = () => {
   return (
     <div>
       <h1>Singular Walkthrough Analytics</h1>
-      <Table style={{ border: "1px solid black" }}>
+      <table>
         <tbody>
           <tr>
-            <td>No. of times walkthrough is launched</td>
-            <td>{singleData.times_launched}</td>
+            <td style={tableborder}>No. of times walkthrough is launched</td>
+            <td style={tableborder}>{singleData.times_launched}</td>
           </tr>
           <tr>
-            <td>No. of times walkthrough is launched (unique users)</td>
-            <td>{singleData.unique_launch}</td>
+            <td style={tableborder}>
+              No. of times walkthrough is launched (unique users)
+            </td>
+            <td style={tableborder}>{singleData.unique_launch}</td>
           </tr>
           <tr>
-            <td>Completion rate</td>
-            <td>{Math.round(singleData.completion_rate * 100)}%</td>
+            <td style={tableborder}>Completion rate</td>
+            <td style={tableborder}>
+              {Math.round(singleData.completion_rate * 100)}%
+            </td>
           </tr>
           <tr>
-            <td>Completion rate (unique users)</td>
-            <td>{Math.round(singleData.unique_completion_rate * 100)}%</td>
+            <td style={tableborder}>Completion rate (unique users)</td>
+            <td style={tableborder}>
+              {Math.round(singleData.unique_completion_rate * 100)}%
+            </td>
           </tr>
           <tr>
-            <td>Dismissal rate</td>
-            <td>{Math.round(singleData.dismissal_rate * 100)}%</td>
+            <td style={tableborder}>Dismissal rate</td>
+            <td style={tableborder}>
+              {Math.round(singleData.dismissal_rate * 100)}%
+            </td>
           </tr>
           <tr>
-            <td>Incomplete walkthrough rate</td>
-            <td>{Math.round(singleData.incomplete_rate * 100)}%</td>
+            <td style={tableborder}>Incomplete walkthrough rate</td>
+            <td style={tableborder}>
+              {Math.round(singleData.incomplete_rate * 100)}%
+            </td>
           </tr>
           <tr>
-            <td>No. of Errors</td>
-            <td>{singleData.errors_count}</td>
+            <td style={tableborder}>No. of Errors</td>
+            <td style={tableborder}>{singleData.errors_count}</td>
           </tr>
         </tbody>
-      </Table>
+      </table>
     </div>
   );
 };
